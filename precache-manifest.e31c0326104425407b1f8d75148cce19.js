71ecbee2cc813d1063c6168626c913d9f998cfdb
xiaodit <31346973+xiaodit@users.noreply.github.com>
2019-10-31 12:43:12 +0000

    Deploying to gh-pages from master bff68519dc6b88ffdb941819b8bda536a99a0b71

---
+++ precache-manifest.e31c0326104425407b1f8d75148cce19.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208708b2fe20e91058b05cf3a0ca00b9",
+    "url": "/avatar-star-react/index.html"
+  },
+  {
+    "revision": "3f728014d047fb09099f",
+    "url": "/avatar-star-react/static/css/2.a2012af5.chunk.css"
+  },
+  {
+    "revision": "1e5d2d62fbb0212c72a7",
+    "url": "/avatar-star-react/static/css/main.fda30a71.chunk.css"
+  },
+  {
+    "revision": "3f728014d047fb09099f",
+    "url": "/avatar-star-react/static/js/2.c99d3352.chunk.js"
+  },
+  {
+    "revision": "1e5d2d62fbb0212c72a7",
+    "url": "/avatar-star-react/static/js/main.959b142a.chunk.js"
+  },
+  {
+    "revision": "14c73a3f4c814e84bb5a",
+    "url": "/avatar-star-react/static/js/runtime-main.8fca7496.js"
+  },
+  {
+    "revision": "969b36c2ce495d58b652b3306d6ddd58",
+    "url": "/avatar-star-react/static/media/style-1.969b36c2.png"
+  },
+  {
+    "revision": "7d07d80f00cc64b368478d4bdb920760",
+    "url": "/avatar-star-react/static/media/style-2.7d07d80f.png"
+  },
+  {
+    "revision": "7e0fd93e3e64c48ce2445ffbc851fd31",
+    "url": "/avatar-star-react/static/media/style-3.7e0fd93e.png"
+  }
+]);
